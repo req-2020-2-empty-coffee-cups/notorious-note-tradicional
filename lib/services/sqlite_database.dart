745c7cb74c5c3144fbeae7c4bc9a4bcff4599af2
Marcos Adriano Nery de Abrantes <marcosadrianonery@gmail.com>
2021-05-13 17:48:17 -0300

    Revert "Uc03 interface melhorada"

--- lib/services/sqlite_database.dart
+++ lib/services/sqlite_database.dart
@@ -18,7 +18,6 @@
     along with NotoriousNote.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:notorious_note/models/task_model.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 
 import '../models/note_model.dart';
@@ -65,18 +64,6 @@ CREATE TABLE IF NOT EXISTS note_tags(
 	FOREIGN KEY (tag_id) REFERENCES tags(id)
 	ON DELETE CASCADE ON UPDATE NO ACTION);
 """);
-
-      await db.execute("""
-      CREATE TABLE IF NOT EXISTS tasks(
-      id INTEGER PRIMARY KEY,
-      content TEXT DEFAULT NULL,
-      done INTEGER DEFAULT 0,
-      creation TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      CHECK(typeof(done) = 'integer'),
-      CHECK(content != ''),
-      CHECK(content IS NOT NULL));
-      """);
-
       result = 0;
       return result;
     }
@@ -169,51 +156,4 @@ CREATE TABLE IF NOT EXISTS note_tags(
     // TODO: implement updateTag
     throw UnimplementedError();
   }
-
-
-
-  @override
-  Future<List<TaskModel>> listTasks() async {
-    List<Map<String, dynamic>> results =
-    await db.rawQuery("SELECT * FROM tasks");
-    print("task $results");
-    return results
-        .map((Map<String, dynamic> map) => TaskModel.fromMap(map))
-        .toList();
-  }
-
-
-  @override
-  Future<TaskModel> readTask( int id) {
-    // TODO: implement readTag
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> updateTask(TaskModel taskModel) async {
-    Map<String, dynamic> taskMap = taskModel.toMap();
-    taskMap.remove("creation");
-    print("try to insert $taskMap");
-    await db
-        .update("tasks", taskMap, where: "id = ?", whereArgs: [taskModel.id]);
-  }
-
-  @override
-  Future<void> deleteTask(int id) async{
-    await db.delete("tasks", where: "id = ?", whereArgs: [id]);
-  }
-
-
-
-
-  @override
-  Future<void> createTask(TaskModel taskModel) async{
-    Map<String, dynamic> taskMap = taskModel.toMap();
-    taskMap.remove("id");
-    taskMap.remove("creation");
-    print("trey to insert $taskMap");
-    await db.insert("tasks", taskMap);
-
-  }
 }
-
